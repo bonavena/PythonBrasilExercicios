d9baaca712aed8e8dd99cff9d8da935555b75597
bonavena <44847901+bonavena@users.noreply.github.com>
2019-02-14 22:13:18 -0200

    Update 02_imprime_numero.py

--- 01_EstruturaSequencial/02_imprime_numero.py
+++ 01_EstruturaSequencial/02_imprime_numero.py
@@ -1,2 +1,7 @@
+'''
+lembrando que nesse caso do INPUT já estamos convertendo para int pois por defaut ele vem como string.
+Aqui está sendo usado o RAW_INPUT por causa da vesão do Python (mais antiga), atualmente usaremos INPUT, beleza? 
+A partir da versão 3.0 usamos INPUT
+'''
 x = int(raw_input("Informe um numero: "))
 print "O numero informado foi:", x
